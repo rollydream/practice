a80190b4b03a2aab72bfab78bd6bbb9ea3eaf681
rollydream <rollydream@hanmail.net>
2016-08-16 20:06:50 +0900

    first commit

---
+++ js/lib/html5shiv.js
@@ -0,0 +1,9 @@
+document.createElement('nav');
+document.createElement('article');
+document.createElement('section');
+document.createElement('aside');
+document.createElement('header');
+document.createElement('footer');
+document.createElement('figcaption');
+document.createElement('figure');
+document.createElement('main');
